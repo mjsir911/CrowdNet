0d0559c0d35a6ba170facad5ab49e53889191653
Marco Sirabella <msirabel@gmail.com>
2017-01-03 18:03:54 -0500

    - Added input loop and pattern set

--- test/threeXNOR.py
+++ test/threeXNOR.py
@@ -16,11 +16,26 @@ __email__       = "msirabel@gmail.com"
 __status__      = "Prototype"  # "Prototype", "Development" or "Production"
 __module__      = ""
 
-network = net.Net(1, 3, [10], 2)
+
+# doesnt like fractions of values, stick with 0s and 1s
+# if two zeros, output 0, 1
+# if two ones, output 1, 0
+# if three zeros or ones, output 1, 1
+pattern = [ [ [1, 1, 1], [1, 1] ], [ [1, 0, 1], [1, 0] ],
+            [ [0, 1, 1], [1, 0] ], [ [0, 0, 1], [0, 1] ],
+
+            [ [1, 1, 0], [1, 0] ], [ [1, 0, 0], [0, 1] ],
+            [ [0, 1, 0], [0, 1] ], [ [0, 0, 0], [1, 1] ],
+            ]
+
+
+network = net.Net(0.1, 3, [20], 2)
 inp = lambda: int(input('a binary value? : '))
 try:
-    network.mass_train(net.t, int(2e3))
+    network.mass_train(pattern, int(2e3))
 except KeyboardInterrupt:
     print('impatient much?')
-network.inputs = [inp(), inp(), inp()]
-print(network.outputs)
+
+while True:
+    network.inputs = [inp(), inp(), inp()]
+    print(network.outputs)
