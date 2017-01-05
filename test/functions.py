1b743c808c2264a525faa94ba9a5c2026888a216
Marco Sirabella <msirabel@gmail.com>
2017-01-05 14:37:37 -0500

    - Added some tests

--- test/functions.py
+++ test/functions.py
@@ -16,15 +16,50 @@ __email__       = "msirabel@gmail.com"
 __status__      = "Prototype"  # "Prototype", "Development" or "Production"
 __module__      = ""
 
-div = lambda a, b, c, d, e, f, g, h: [int(z) for z in list(format(int(int(str(round(a)) + str(round(b)) + str(round(c)) + str(round(d)), 2) / int(str(round(e)) + str(round(f)) + str(round(g)) + str(round(h)), 2)), '04b'))]
+base = 4
 
-divfull = net.Net(0.1, 8, [6], 4)
+#div = lambda *args: [int(z) for z in list(format(int(''.join(str(round(n)) for n in args[:base]), 2) // int(''.join(str(round(n)) for n in args[base:]), 2), '0{}b'.format(base)))]
+
+def mad(func):
+    def wrapper(*args, **kwargs):
+        first = int(''.join(str(round(n)) for n in args[:base]), 2)
+        last = int(''.join(str(round(n)) for n in args[base:]), 2)
+        input_bin = [int(z) for z in list(format(func(first, last), '0{}b'.format(base)))]
+        return input_bin
+    return wrapper
+
+add = mad(lambda a, b: a + b)
+sub = mad(lambda f, l: f - l)
+mul = mad(lambda a, b: a * b)
+div = mad(lambda d, n: d // n)
+
+simple = 0
+inverse = 1
+add_op = (0, 0)
+sub_op = (0, 1)
+mul_op = (1, 0)
+div_op = (1, 1)
+
+"""
+def math(*args):
+    op = args[:2]
+    args = args[2:]
+    if op[1] == simple:
+        if op[1] != inverse:
+
+    else:
+        if op[1] != inverse:
+            """
+
+
+#divfull = net.Net(0.1, base * 2 + 2, [base + 6], base)
+divfull = net.Net(0.1, base * 2, [base + 4], base)
 
 divfull.function_train(div, 1e6)
 
 def division(n, d):
-    n = [int(num) for num in list(format(n, '04b'))]
-    d = [int(num) for num in list(format(d, '04b'))]
+    n = [int(num) for num in list(format(n, '0{}b'.format(base)))]
+    d = [int(num) for num in list(format(d, '0{}b'.format(base)))]
     divfull.inputs = n + d
     o = int(''.join([str(round(num)) for num in divfull.outputs]), 2)
     return o
--- test/threeXNOR.py
+++ test/threeXNOR.py
@@ -18,9 +18,11 @@ __module__      = ""
 
 
 # doesnt like fractions of values, stick with 0s and 1s
-# if two zeros, output 0, 1
-# if two ones, output 1, 0
-# if three zeros or ones, output 1, 1
+# if two zeros, output 0
+# if two ones, output 0
+# if three zeros or ones, output 1
+
+# this doesnt actually learn it seems
 pattern = [ [ [1, 1, 1], [1, 1] ], [ [1, 0, 1], [1, 0] ],
             [ [0, 1, 1], [1, 0] ], [ [0, 0, 1], [0, 1] ],
 
@@ -28,13 +30,24 @@ pattern = [ [ [1, 1, 1], [1, 1] ], [ [1, 0, 1], [1, 0] ],
             [ [0, 1, 0], [0, 1] ], [ [0, 0, 0], [1, 1] ],
             ]
 
+"""
+pattern = [ [ [1, 1, 1], [1] ], [ [1, 0, 1], [0] ],
+            [ [0, 1, 1], [0] ], [ [0, 0, 1], [0] ],
 
-network = net.Net(0.1, 3, [20], 2)
+            [ [1, 1, 0], [0] ], [ [1, 0, 0], [0] ],
+            [ [0, 1, 0], [0] ], [ [0, 0, 0], [1] ],
+            ]
+            """
+
+
+network = net.Net(0.001, 3, [3], 2)
 inp = lambda: int(input('a binary value? : '))
 try:
-    network.mass_train(pattern, int(2e3))
+    network.mass_train(pattern, int(2e50))
 except KeyboardInterrupt:
     print('impatient much?')
+
+print([[axon.value for axon in neuron.axons] for neuron in network.neurons])
 
 while True:
     network.inputs = [inp(), inp(), inp()]
