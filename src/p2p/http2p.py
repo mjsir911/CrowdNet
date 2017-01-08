a310c64b437e9bc08968bc3638115722d3bf5b2f
Marco Sirabella <msirabel@gmail.com>
2017-01-08 08:43:56 -0500

    - Added local ip address binding

--- src/p2p/http2p.py
+++ src/p2p/http2p.py
@@ -55,7 +55,7 @@ class Server():
         self.Request.add_get_response(['dill'], lambda s: cortex.introspect(self._obj))
         self.Request.add_get_response(['phonebook'], lambda s: cortex.introspect(self.phonebook))
 
-        self.httpd = http.server.HTTPServer(self.address, self.Request)
+        self.httpd = http.server.HTTPServer(address, self.Request)
         self.httpd.request_queue_size = 100
         self.serve()
         self.equalize()
