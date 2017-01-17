3882640d0ea9c6bd575bf5090ae05d85ed141348
Marco Sirabella <msirabel@gmail.com>
2017-01-17 16:13:04 -0500

    - Added bitarray for audio

--- src/test.py
+++ src/test.py
@@ -3,6 +3,7 @@
 
 import time
 import multiprocessing
+import collections
 
 __appname__     = ""
 __author__      = "Marco Sirabella"
@@ -78,6 +79,13 @@ def pool(funcs):
     p = multiprocessing.Pool()
 
 
+def chunks(l, n):
+    """Yield successive n-sized chunks from l."""
+    """http://stackoverflow.com/questions/312443/how-do-you-split-a-list-into-evenly-sized-chunks"""
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
 
 def truey(l):
 #print(l)
@@ -101,3 +109,104 @@ def mad(base=4):
             return input_bin
         return wrapper
     return decorator
+
+
+#this is a mess
+class bitarray(list):
+    def __init__(self, iterable=None):
+        #super().__init__(bytes)
+        super().__init__()
+        if iterable:
+            for item in iterable:
+                self.append(item)
+
+    def __repr__(self):
+        return 'bitarray({})'.format(self)
+
+    def __str__(self):
+        x = '0b'
+        for bit in self:
+            x += str(bit)
+        return x
+
+    def __setitem__(self, index, value):
+        super().__setitem__(index, self._value_bin(value))
+
+    @classmethod
+    def _value_bin(cls, value):
+        if isinstance(value, int):
+            if 0 <= value <= 1:
+                return value
+            else:
+                raise ValueError('bit must be in range(0, 1)')
+        else:
+            raise ValueError('an integer is required')
+
+    def _binary(func):
+        def wrapper(self, value):
+            bin = self._value_bin(value)
+            return func(self, bin)
+        return wrapper
+
+    @classmethod
+    def _iterable_bin(cls, iterable):
+        buffer = []
+        for value in iterable:
+            buffer.append(cls._value_bin(value))
+        return buffer
+
+    def _iterator(func):
+        def wrapper(self, object):
+            iterable = self._iterable_bin(object)
+            return func(self, iterable)
+        return wrapper
+
+
+
+    @_binary
+    def append(self, value):
+        super().append(value)
+
+    @_iterator
+    def extend(self, iterable):
+        super().extend(iterable)
+
+    def insert(self, index, value):
+        super().insert(index, self._value_bin(value))
+
+
+    def _math(func):
+        def wrapper(self, object):
+            return type(self)(func(self, object))
+        return wrapper
+
+    @_math
+    @_iterator
+    def __add__(self, obj):
+        return super().__add__(obj)
+    @_math
+    @_iterator
+    def __radd__(self, obj):
+        return super().__radd__(obj)
+
+    @_iterator
+    def __iadd__(self, obj):
+        return super().__iadd__(obj)
+
+    @_math
+    def __mul__(self, obj):
+        return super().__mul__(obj)
+    @_math
+    def __rmul__(self, obj):
+        return super().__rmul__(obj)
+    def __imul__(self, obj):
+        return super().__imul__(obj)
+
+    def convert_to_base(self, base):
+        converted = []
+        for i in range(0, len(self), base):
+            thing = ''
+            for j in range(0, base):
+                thing += (str(self[j + i]))
+            converted.append(int(thing, 2))
+        return converted
