dabcbc14acd2c466d6b52e7e748aa0e630624fbe
Marco Sirabella <msirabel@gmail.com>
2017-04-20 18:05:20 -0400

    - Started images again

---
+++ test/digits/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import PIL.Image
+
+__appname__     = ""
+__author__      = "Marco Sirabella"
+__copyright__   = ""
+__credits__     = ["Marco Sirabella"]  # Authors and bug reporters
+__license__     = "GPL"
+__version__     = "1.0"
+__maintainers__ = "Marco Sirabella"
+__email__       = "msirabel@gmail.com"
+__status__      = "Prototype"  # "Prototype", "Development" or "Production"
+__module__      = ""
+
+class Pic(bytes, PIL.Image)
